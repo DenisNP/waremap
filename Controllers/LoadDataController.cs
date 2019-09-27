6acd6d823a6e7b874dd7fbcf04a99f587317db3e
Denis Peshekhonov <denis@peshekhonov.ru>
2019-09-27 20:51:59 +0300

    cars and parts loaded

--- Controllers/LoadDataController.cs
+++ Controllers/LoadDataController.cs
@@ -15,23 +15,45 @@ namespace Waremap.Controllers
         [HttpPost]
         public string Load()
         {
+            if (!Request.Query.ContainsKey("data"))
+            {
+                return "no data field";
+            }
+            
+            var data = Request.Query["data"];
             try
             {
-                using (var reader = new StreamReader(Request.Body))
+                using var reader = new StreamReader(Request.Body);
+                switch (data)
                 {
-                    var carsList = JsonConvert.DeserializeObject<List<Car>>(reader.ReadToEnd());
-                    LoadCarsToState(carsList, ReceiveEventController.GetState());
-                    return "ok";
+                    case "cars":
+                        LoadCarsToState(reader.ReadToEnd(), ReceiveEventController.GetState());
+                        return "ok";
+                    case "parts":
+                        LoadPartsToState(reader.ReadToEnd(), ReceiveEventController.GetState());
+                        return "ok";
+                    default:
+                        return "wrong data";
                 }
             }
             catch (Exception e)
             {
+                Console.WriteLine(e.ToString());
                 return "bad";
             }
         }
 
-        public static void LoadCarsToState(List<Car> cars, State state)
+        public static void LoadPartsToState(string json, State state)
         {
+            var parts = JsonConvert.DeserializeObject<List<Part>>(json, Utils.ConverterSettings);
+            var existIds = state.Equipment.Parts.Select(p => p.Id);
+            var newCars = parts.Where(p => !existIds.Contains(p.Id));
+            state.Equipment.Parts.AddRange(newCars);
+        }
+
+        public static void LoadCarsToState(string json, State state)
+        {
+            var cars = JsonConvert.DeserializeObject<List<Car>>(json, Utils.ConverterSettings);
             var existIds = state.Equipment.Cars.Select(c => c.Id);
             var newCars = cars.Where(c => !existIds.Contains(c.Id));
             state.Equipment.Cars.AddRange(newCars);
