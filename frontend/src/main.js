43671f2409d1d337661d01f7201ae186d03b96aa
Vladimir Muromskiy <aiditz@yandex.ru>
2019-09-27 20:26:02 +0300

    Add front

---
+++ frontend/src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import App from './App.vue';
+import store from './store';
+import './registerServiceWorker';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+Vue.config.productionTip = false;
+
+new Vue({
+  store,
+  render: h => h(App),
+}).$mount('#app');
