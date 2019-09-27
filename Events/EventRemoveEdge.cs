6885aba23fc1e7597dca19e2fa89302c69d49228
Grigory Frantsuzov <gfrantsuzov@systemorph.com>
2019-09-27 18:35:29 +0300

    add edge, remove edge

---
+++ Events/EventRemoveEdge.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Waremap.Models;
+
+namespace Waremap.Events
+{
+    public class EventRemoveEdge: IEvent
+    {
+        private int _from;
+        private int _to;
+
+        public EventRemoveEdge(int from, int to)
+        {
+            _from = from;
+            _to = to;
+        }
+
+        public void Run(State state)
+        {
+            var edge = state.Geo.Edges.FirstOrDefault(x => x.To == _to && x.From == _from);
+            if (edge != null)
+            {
+                state.Geo.Edges.Remove(edge);
+            }
+        }
+    }
+}
