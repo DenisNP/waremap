84899e3d80294b7e0598cbeb3e49563a425fad76
Vladimir Muromskiy <aiditz@yandex.ru>
2019-09-27 20:50:06 +0300

    Add files to front

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DepotEditor/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import DepotEditor from './components/DepotEditor.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    DepotEditor,
   },
 };
 </script>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,16 +1,41 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import helpers from './common/helpers';
+import config from './common/config';
+import API from './common/api';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  modules: {
+  },
   state: {
-
+  },
+  getters: {
   },
   mutations: {
-
+    set(state, {field, value}) {
+      state[field] = value;
+    },
   },
   actions: {
+    async init(context) {
+      let data;
+      let initNeeded = true;
 
-  },
+      try {
+        data = await API.startGame();
+      } catch (err) {
+        data = require('./common/backend-response.dist.json');
+        initNeeded = false;
+        console.warn('TEST DATA USED');
+      }
+
+      if (!initNeeded) {
+        return;
+      }
+
+      // set state from data
+    },
+  }
 });
