368b2650557ef936d1041021e245f8e83340e428
Grigory Frantsuzov <gfrantsuzov@systemorph.com>
2019-09-29 01:43:47 +0300

    saved state class

---
+++ Models/SavedState.cs
@@ -0,0 +1,14 @@
+﻿namespace Waremap.Models
+{
+    public class SavedState
+    {
+        public State State { get; set; }
+        public Background[] Backgrounds { get; set; }
+    }
+
+    public class Background
+    {
+        public int Floor { get; set; }
+        public string Base64 { get; set; }
+    }
+}
