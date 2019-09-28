5bec390cd23dd3270ee48c9f5b683cdef4cf8ed5
Denis Peshekhonov <denis@peshekhonov.ru>
2019-09-28 20:01:32 +0300

    position try

--- Controllers/PositionController.cs
+++ Controllers/PositionController.cs
@@ -33,43 +33,60 @@ namespace Waremap.Controllers
 
         [HttpGet]
         public string GetPosition()
-        {  
-            return JsonConvert.SerializeObject(Position(), Utils.ConverterSettings);
+        {
+            try
+            {
+                return JsonConvert.SerializeObject(Position(), Utils.ConverterSettings);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
 
         [HttpPost]
         public string PostPosition([FromBody] Presence presence)
         {
-            var state = ReceiveEventController.GetState();
-            var part = state.Equipment.Parts.FirstOrDefault(x => x.Id == presence.PartId);
-            var machineNode = state.Geo.Nodes.FirstOrDefault(x => x.Id == presence.MachineId);
-
-            if (machineNode == null || machineNode.Type != NodeType.Machine || part == null) 
-                return "No machine or part found";
-
-            var potentialWaypoint = state.CarWaypoints
-                .FirstOrDefault(
-                    wp => wp.FromNode == presence.MachineId 
-                          && wp.ToNode == presence.MachineId
-                          && part.Path.Select(process => process.Id).Contains(presence.OperationId)
-                );
-            
-            if (potentialWaypoint != null) {
-                state.CarPosition = state.CarWaypoints.IndexOf(potentialWaypoint);
-                part.Waypoint = potentialWaypoint;
-                return GetPosition();
-            }
-            
-            // new waypoint
-            part.Waypoint = new Waypoint
+            try
             {
-                FromNode = presence.MachineId,
-                ToNode = presence.MachineId,
-                OperationId = presence.OperationId,
-                OffWay = true
-            };
-            state.CarWaypoints.Insert(state.CarPosition + 1, part.Waypoint);
-            return JsonConvert.SerializeObject(Position(true), Utils.ConverterSettings);
+                var state = ReceiveEventController.GetState();
+                var part = state.Equipment.Parts.FirstOrDefault(x => x.Id == presence.PartId);
+                var machineNode = state.Geo.Nodes.FirstOrDefault(x => x.Id == presence.MachineId);
+
+                if (machineNode == null || machineNode.Type != NodeType.Machine || part == null)
+                    return "No machine or part found";
+
+                var potentialWaypoint = state.CarWaypoints
+                    .FirstOrDefault(
+                        wp => wp.FromNode == presence.MachineId
+                              && wp.ToNode == presence.MachineId
+                              && part.Path.Select(process => process.Id).Contains(presence.OperationId)
+                    );
+
+                if (potentialWaypoint != null)
+                {
+                    state.CarPosition = state.CarWaypoints.IndexOf(potentialWaypoint);
+                    part.Waypoint = potentialWaypoint;
+                    return GetPosition();
+                }
+
+                // new waypoint
+                part.Waypoint = new Waypoint
+                {
+                    FromNode = presence.MachineId,
+                    ToNode = presence.MachineId,
+                    OperationId = presence.OperationId,
+                    OffWay = true
+                };
+                state.CarWaypoints.Insert(state.CarPosition + 1, part.Waypoint);
+                return JsonConvert.SerializeObject(Position(true), Utils.ConverterSettings);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
 
         public PositionResult Position(bool offWay = false)
