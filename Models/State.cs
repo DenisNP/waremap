3921a8242495878a6b7849e8fd09a9607507f623
Denis Peshekhonov <denis@peshekhonov.ru>
2019-09-28 11:26:36 +0300

    background controller

--- Models/State.cs
+++ Models/State.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Waremap.Models
 {
@@ -6,6 +7,9 @@ namespace Waremap.Models
     {
         public Geo Geo { get; set; } = new Geo();
         public Equipment Equipment { get; set; } = new Equipment();
+        
+        [JsonIgnore]
+        public string Background { get; set; }
     }
     
     public class Node
