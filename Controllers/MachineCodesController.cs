823871779eb221dee10950b334e952b0f8b1d582
Denis Peshekhonov <denis@peshekhonov.ru>
2019-09-28 19:43:21 +0300

    small

--- Controllers/MachineCodesController.cs
+++ Controllers/MachineCodesController.cs
@@ -11,34 +11,36 @@ namespace Waremap.Controllers
         [HttpGet]
         public string Get()
         {
-            if (Request.Query.ContainsKey("id") && int.TryParse(Request.Query["id"], out var nodeId))
+            if (!Request.Query.ContainsKey("id") || !int.TryParse(Request.Query["id"], out var nodeId))
             {
-                var state = ReceiveEventController.GetState();
-                var node = state.Geo.Nodes.FirstOrDefault(n => n.Id == nodeId);
-                if (node != null)
-                {
-                    var parts = state.Equipment.Parts.Where(
-                        p => p.Path.Any(process => node.OperationIds.Contains(process.Id))
-                    );
-
-                    var awaitingParts = parts.Select(p =>
-                    {
-                        var operation = state.Equipment.Operations.FirstOrDefault(o => o.Id == node.OperationIds.First());
-                        return new AwaitingPart
-                        {
-                            PartId = p.Id,
-                            MachineId = nodeId,
-                            PartName = p.Name,
-                            OperationId = operation?.Id ?? 0,
-                            OperationName = operation?.Name ?? ""
-                        };
-                    });
-
-                    return JsonConvert.SerializeObject(awaitingParts, Utils.ConverterSettings);
-                }
+                return "No correct id specified";
             }
 
-            return "No correct id specified";
+            var state = ReceiveEventController.GetState();
+            var node = state.Geo.Nodes.FirstOrDefault(n => n.Id == nodeId);
+            if (node == null)
+            {
+                return "No correct id specified";
+            }
+            
+            var parts = state.Equipment.Parts.Where(
+                p => p.Path.Any(process => node.OperationIds.Contains(process.Id))
+            );
+
+            var awaitingParts = parts.Select(p =>
+            {
+                var operation = state.Equipment.Operations.FirstOrDefault(o => o.Id == node.OperationIds.First());
+                return new AwaitingPart
+                {
+                    PartId = p.Id,
+                    MachineId = nodeId,
+                    PartName = p.Name,
+                    OperationId = operation?.Id ?? 0,
+                    OperationName = operation?.Name ?? ""
+                };
+            });
+
+            return JsonConvert.SerializeObject(awaitingParts, Utils.ConverterSettings);
         }
 
         private struct AwaitingPart
