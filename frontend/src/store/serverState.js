dfe7f1f539899e01445b73253db8327cab6517a0
Vladimir Muromskiy <aiditz@yandex.ru>
2019-09-28 22:39:37 +0300

    xxx

--- frontend/src/store/serverState.js
+++ frontend/src/store/serverState.js
@@ -1,3 +1,4 @@
+import API from '../common/api';
 import Vue from 'vue';
 
 export default {
@@ -36,8 +37,26 @@ export default {
       }
 
       Vue.set(state.geo.nodes, nodeIndex, {...state.geo.nodes[nodeIndex], ...newNode});
-    }
+    },
   },
   actions: {
+    async importFullState(c, fullState) {
+      debugger;
+      /*
+      const res = await Promise.all([
+        API.sendAction('load', fullState.state),
+        ...fullState.backgrounds.filter(Boolean).map(bg =>
+          API.sendBackground(bg.base64, bg.floor),
+        )
+      ]);
+      */
+      const newState = await API.sendAction('load', fullState.state);
+
+      for (const bg of fullState.backgrounds) {
+        await API.sendBackground(bg.base64, bg.floor);
+      }
+
+      c.commit('setServerState', newState, {root: true});
+    },
   }
 };
