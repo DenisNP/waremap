4ff6af9655172c78cdbf7eb5d5410c034d161b2a
Vladimir Muromskiy <aiditz@yandex.ru>
2019-09-29 04:09:10 +0300

    operation list

---
+++ frontend/src/components/OperationList.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <li v-for="item in data">{{ item.name }}</li>
+  </ul>
+</template>
+
+<script>
+
+export default {
+  name: 'OperationsList',
+  props: [
+    'data'
+  ],
+  data() {
+  },
+  methods: {
+  },
+  mounted() {
+  },
+}
+
+</script>
+
+<style>
+
+</style>
